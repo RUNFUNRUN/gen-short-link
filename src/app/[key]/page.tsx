4f6edcf6fdf5ac67dafe5d9b31df830247ce2212
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2025-02-15 00:02:59 +0900

    fix: kv variable name

--- src/app/[key]/page.tsx
+++ src/app/[key]/page.tsx
@@ -5,8 +5,8 @@ const Page = async (props: { params: Promise<{ key: string }> }) => {
   const params = await props.params;
   const key = params.key;
   const { env } = getCloudflareContext();
-  const kvs = env.LINK_KV;
-  const url = await kvs.get(key);
+  const kv = env.LINK_KV;
+  const url = await kv.get(key);
   if (!url) {
     redirect('/');
   }
